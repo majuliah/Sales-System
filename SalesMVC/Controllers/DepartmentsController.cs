ddd82c91ceee41a0e84b375a1b2e6ab9175dfbb7
majulha <majulialcarvalho@gmail.com>
2023-05-25 14:17:11 -0300

    first mvc: Department

---
+++ SalesMVC/Controllers/DepartmentsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesMVC.Models;
+
+namespace SalesMVC.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> departments = new List<Department>();
+            departments.Add(new Department{ Id = 001, Name = "Eletronics" });
+            departments.Add(new Department{ Id = 002, Name = "Fashion" });
+            departments.Add(new Department{ Id = 003, Name = "Games" });
+
+            return View(departments);
+        }
+    }
+}
--- SalesMVC/Controllers/HomeController.cs
+++ SalesMVC/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using SalesMVC.Models.ViewModels;
 
 namespace SalesMVC.Controllers
 {
@@ -18,6 +19,7 @@ namespace SalesMVC.Controllers
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
+            ViewData["Email"] = "pageEmail@outlook.com";
 
             return View();
         }
