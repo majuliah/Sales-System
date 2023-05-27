3a30e4b8046b930bfa6ffe11a8ae7e0cb91accdf
majulha <majulialcarvalho@gmail.com>
2023-05-26 22:49:51 -0300

    updating exceptions

--- SalesMVC/Services/SellerService.cs
+++ SalesMVC/Services/SellerService.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using SalesMVC.Data;
 using SalesMVC.Models;
 using Microsoft.EntityFrameworkCore;
+using SalesMVC.Services.Exceptions;
 
 namespace SalesMVC.Services
 {
@@ -36,6 +37,24 @@ namespace SalesMVC.Services
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+
+        public void Update(Seller obj)
+        {
+            if(!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id not found");
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch(DbConcurrencyException exception)
+            {
+                throw new DbConcurrencyException(exception.Message);
+            }
+
         }
     }
 }
